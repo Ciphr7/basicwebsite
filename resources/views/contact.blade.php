a39e193d04e52e37ebb2a6ef9a752184d1454056
Gil <Gil@psdc.com>
2019-12-11 15:08:40 -0600

    first push

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('content')
+
+<h1>Contact</h1>
+
+@endsection
+
+@section('sidebar')
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+
+@extends('layout.app')
+
+@section('content')
+
+<h1>Home</h1>
+
+@endsection
+
+@section('sidebar')
+
+  @parent
+
+  This is a home page append to sidebar
+
+@endsection
